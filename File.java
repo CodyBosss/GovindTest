8308d2c4220958326c210c37fb5429e27966fe0a
ronny <ronny@gmail.com>
2025-05-13 14:21:44 +0530

    Second commint

--- File.java
+++ File.java
@@ -1,7 +1,8 @@
 package NewProject;
 
 public class File {
-    public static void main(String[] args){
+    public static void main(String[] args)     {
         System.out.println("Hello");
+        System.out.println("Tatya");
     }
 }
